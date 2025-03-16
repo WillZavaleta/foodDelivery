c3bdc418249444af26cfa6d6de230935b411cede
WillZavaleta <122190811+WillZavaleta@users.noreply.github.com>
2025-03-15 23:14:33 -0600

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ import Stripe from "stripe";
 // placing user order for frontend
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://tomato-t8yt.onrender.com/";
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
     try {
