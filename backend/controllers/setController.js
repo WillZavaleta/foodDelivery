4455e8d224b24ebe53e089c6bcddc90312eb525a
Will <zavaleta.will@gmail.com>
2025-03-18 03:41:56 -0600

    tarifa

--- backend/controllers/setController.js
+++ backend/controllers/setController.js
@@ -2,6 +2,7 @@ import setModel from "../models/setModel.js";
 
 const updateTarifa = async (req,res) => {
     try {
+        console.log("tarifa:",req.body.tarifa)
         await setModel.findOneAndUpdate({},{tarifa:req.body.tarifa})
         res.json({success:true,message:"Tarifa actualizada"})
     } catch (error) {
